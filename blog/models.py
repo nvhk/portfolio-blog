b5d0f342dbd022c665dfaa95d207040fdbbee59d
RV <rafal.piwnicki@gmail.com>
2018-11-11 21:28:18 +0100

    Blog

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    tytul = models.CharField(max_length=100)
+    data = models.DateField(auto_now=True)
+    tekst = models.TextField()
+    obrazekbloga = models.ImageField(upload_to='obrazki/')
