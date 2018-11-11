47fb2df3e7cbac23d34321548b1bf899aa4d0311
RV <rafal.piwnicki@gmail.com>
2018-11-11 18:18:08 +0100

    Prace model - create

--- prace/models.py
+++ prace/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Praca(models.Model):
+    obrazek = models.ImageField(upload_to='obrazki/')
+    podpis = models.CharField(max_length=245)
