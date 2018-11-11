6c86faa5365b53b93a53191ffbd115f574331374
RV <rafal.piwnicki@gmail.com>
2018-11-11 21:38:48 +0100

    Data fixed migrate

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,6 @@ from django.db import models
 # Create your models here.
 class Blog(models.Model):
     tytul = models.CharField(max_length=100)
-    data = models.DateField(auto_now=True)
+    data = models.DateTimeField(auto_now_add=False)
     tekst = models.TextField()
     obrazekbloga = models.ImageField(upload_to='obrazki/')
