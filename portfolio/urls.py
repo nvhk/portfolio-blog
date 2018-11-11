fd7577062dce3936eb5f358b3fded75b3a03c5c5
RV <rafal.piwnicki@gmail.com>
2018-11-11 22:07:05 +0100

    url patchs

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -17,7 +17,12 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+import prace.views
+import blog.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', prace.views.home, name='home'),
+    path('blog/', blog.views.blog, name='blog')
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
